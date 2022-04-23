8ec3dfb74d43e5481168660731b11697475e8c58
daykim <45174177+ekdud0529@users.noreply.github.com>
2022-04-23 13:41:53 +0900

    add libft folder

--- ft_printf.c
+++ ft_printf.c
@@ -19,10 +19,57 @@ int	ft_printf(const char *format, ...)
 
 	cnt = 0;
 	va_start(ap, format);
-	while(format)
+	while(format[cnt])
 	{
-
+		if (format[cnt] != '%')
+		{
+			cnt++;
+			// 출력
+			cnt++;
+		}
+		else
+		{
+			cnt += check_type((char)format[cnt]);
+		}
 	}
 	va_end(ap);
 	return (cnt);
+}
+
+int	check_type(char	conv)
+{
+	if(conv == 'c' || conv == '%')
+	{
+		
+	}
+	else if(conv == 's')
+	{
+
+	}
+	else if(conv == 'p')
+	{
+
+	}
+	else if(conv == 'd' || conv == 'i')
+	{
+
+	}
+	else if(conv == 'i')
+	{
+
+	}
+	else if(conv == 'u')
+	{
+
+	}
+	else if(conv == 'x')
+	{
+
+	}
+	else if(conv == 'X')
+	{
+
+	}
+	else
+		return (0);
 }
